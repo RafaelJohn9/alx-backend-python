8bb3ab8998b99c6343f750eab85171e2010abdd6
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-11 18:11:57 +0300

    2.Basic annotations-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a typed annotation function floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    it returns the whole number part of the parameter
+    """
+    return int(n)
