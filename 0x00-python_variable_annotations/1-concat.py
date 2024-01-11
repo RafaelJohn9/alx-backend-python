a0cd5610bb8dafabaa6d2476b31fcd9623646566
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-11 18:07:31 +0300

    1.Basic annotations-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a concat annotated function
+"""
+
+
+def concat(str1:str, str2:str) -> str:
+    """
+    a function that concats two strings
+    """
+    return str1 + str2
