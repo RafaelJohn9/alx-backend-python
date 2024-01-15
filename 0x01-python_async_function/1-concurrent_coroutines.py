10fc205dfc0d930239f8e8ff27e50081802ecba8
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-15 19:56:40 +0300

    1.fixed error

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,4 +13,4 @@ async def wait_n(n: int, max_delay: int) -> List:
     wait_random = __import__('0-basic_async_syntax').wait_random
 
     timings = [wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    return [await timing for timing in asyncio.as_completed(timings)]
