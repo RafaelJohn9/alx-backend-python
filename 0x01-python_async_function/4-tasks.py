f95998e4a7f7b808b7f081ad1079754c14acaa87
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-15 20:00:34 +0300

    4.corrected annotation and code on arrangement on return

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,12 +6,11 @@ import asyncio
 from typing import List
 
 
-async def task_wait_n(n: int, max_delay: int) -> List:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     a function that spawns wait_random n times with the specified max_delay
     """
     task_wait_random = __import__('3-tasks').task_wait_random
 
     timings = [task_wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*timings)
-    return sorted(delays)
+    return [await timing for timing in asyncio.as_completed(timings)]
