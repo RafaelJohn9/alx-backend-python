1744570cfa0c15a0a54417c40cb06e0a19173972
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-15 09:11:40 +0300

    0.type annotation and pycodestyle  && 1.Let's execute multiple coroutines at the same time with async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,12 +6,12 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     takes in an integer argument, with a default value of 10
     named wait_random that waits for a random delay between 0
     and max_delay and eventually returns it
     """
-    randomValue = random.uniform(0, max_delay + 0.1)
+    randomValue = random.uniform(0, max_delay)
     await asyncio.sleep(randomValue)
     return randomValue
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a function that exectues an async function n times
+"""
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List:
+    """
+    a function that spawns wait_random n times with the specified max_delay
+    """
+    wait_random = __import__('0-basic_async_syntax').wait_random
+
+    timings = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*timings)
+    return sorted(delays)
