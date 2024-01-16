e6e66e33d34c1324cb09f8324f02729b7ca8d9b5
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-16 13:52:57 +0300

    0.added typed annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,13 @@ a coroutined that takes no arg
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     loops 10 times and yield a rand value after each loop
     """
-
     for _ in range(10):
         await asyncio.sleep(1)
         yield(random.uniform(0, 10))
