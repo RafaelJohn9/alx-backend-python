0c58f5ddcba35d85e2ef9db1af8c55088fb16e7f
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-04 14:20:30 +0300

    3.parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,10 +10,10 @@ from typing import (
         Dict,
         Callable
         )
-import unittest
 from unittest import TestCase
 from unittest.mock import patch, Mock
 import utils
+from utils import memoize
 
 
 class TestAccessNestedMap(TestCase):
@@ -53,7 +53,7 @@ class TestAccessNestedMap(TestCase):
             utils.access_nested_map(nested_map, path)
 
 
-class TestGetJson(unittest.TestCase):
+class TestGetJson(TestCase):
     """
     tests the getjson function
     """
@@ -71,3 +71,28 @@ class TestGetJson(unittest.TestCase):
             output = utils.get_json(url)
         mocked_get.assert_called_once_with(url)
         self.assertEqual(output, response_json)
+
+class TestMemoize(TestCase):
+    """
+    tests the memoization class
+    """
+    def test_memoize(self):
+        """
+        tests memoization decorator
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            instance = TestClass()
+            result_1 = instance.a_property
+            result_2 = instance.a_property
+
+        mock_method.assert_called_once()
+        self.assertEqual(result_1, 42)
+        self.assertEqual(result_2, 42)
