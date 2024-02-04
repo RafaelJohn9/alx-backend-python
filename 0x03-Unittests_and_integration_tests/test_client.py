6f35c2a72ac3b29cee56caa3347b4bce255ed74a
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-04 20:55:45 +0300

    5.mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 tests the client module
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 import client
 from client import GithubOrgClient
@@ -18,11 +18,27 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc")
         ])
     @patch('client.get_json', return_value={'org': 'dummy'})
-    def test_org(self, org, magic_mock):
+    def test_org(self, org, magicMock):
         """
         tests if org is working well
         """
         org_client = GithubOrgClient(org)
         result = org_client.org
         self.assertEqual(result, {'org': 'dummy'})
-        magic_mock.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+        magicMock.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+
+    def test_public_repos_url(self):
+        """
+        test for the property_public_repos_url
+        """
+        with patch(
+                   "client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock
+                   ) as mock_property:
+            # set desired return value
+            mock_property.return_value = 10
+
+            my_instance = GithubOrgClient("google")
+            result = my_instance._public_repos_url
+
+        self.assertEqual(result, 10)
